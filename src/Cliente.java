2dc330cc80938b44fbffb0c5197ebd5d6c8c7afa
lucasnalua2506 <lucasnalua25@gmail.com>
2020-03-06 21:43:03 -0300

    validar cpf

---
+++ src/Cliente.java
@@ -0,0 +1,26 @@
+import java.util.Date;
+
+public class Cliente {
+	public static void main(String[] args) {
+		String Nome;
+		String Email;
+		String Telefone;
+		Date   Nascimento;
+		String    CPF;
+		
+	}
+	public boolean ehMaiorIdade() {
+		return true;
+	}
+}
+
+
+	
+
+	
+		 
+	
+	
+	
+	
+
